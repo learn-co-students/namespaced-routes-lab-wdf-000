4f67d53687f3c602b6fcd5efadaf741fac02007a
sami4thewin <samiomarmohammed@gmail.com>
2016-10-06 10:21:46 -0400

    Done.

--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe ArtistsController do
   describe "GET new" do
     it "redirects when access is turned off" do
       p = Preference.create(allow_create_artists: false)
+      # binding.pry
       get :new
       expect(response).to redirect_to artists_path
     end
