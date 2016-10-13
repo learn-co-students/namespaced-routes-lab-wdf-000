7b6266209d837c47a8666cf7eb6aa87985a5bacd
Naveed Shah <naveed@tayybah.com>
2016-10-13 13:39:28 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    p = Preference.create(allow_create_artists: false)
+    if p == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
