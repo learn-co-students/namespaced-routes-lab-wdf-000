329c1d82329835c0c0d1addc1a0f9cea7889747d
zenglue <keonilrath@gmail.com>
2016-12-09 21:28:22 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    pref = Preference.create
+    if pref.allow_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path, alert: "Creation not allowed"
+    end
   end
 
   def create
